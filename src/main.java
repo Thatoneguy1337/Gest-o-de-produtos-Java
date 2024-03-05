7e08ef9bd707111c0e9c55ba246f06666dd19664
Victor Guterres <Vgb1337@gmail.com>
2024-03-05 11:21:42 -0300

    finished x tasks

--- src/main.java
+++ src/main.java
@@ -1,4 +1,26 @@
+import product.ProductModel;
+
 public class main {
+
+ public static void Main(String[] args) {
+        
+        ProductModel product = new ProductModel();
+
+        
+        product.setBarCode("123456789");
+        product.setName("Product Name");
+        product.setPriceInCents(1000);
+        product.setStock(10);
+
+        
+    
+        System.out.println("Barcode: " + product.getBarCode());
+        System.out.println("Name: " + product.getName());
+        System.out.println("Price in cents: " + product.getPriceInCents());
+        System.out.println("Stock: " + product.getStock());
+    }    
+
+
 
 
 };
